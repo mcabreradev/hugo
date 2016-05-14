04d9a07f69447649214804427ddcb75bfaac0851
Alexandre Bourget <alex@bourget.cc>
2016-05-14 00:35:16 -0400

    Add multilingual support in Hugo
    Implements:
    * support to render:
      * content/post/whatever.en.md to /en/2015/12/22/whatever/index.html
      * content/post/whatever.fr.md to /fr/2015/12/22/whatever/index.html
    * gets enabled when `Multilingual:` is specified in config.
    * support having language switchers in templates, that know
      where the translated page is (with .Page.Translations)
      (when you're on /en/about/, you can have a "Francais" link pointing to
       /fr/a-propos/)
      * all translations are in the `.Page.Translations` map, including the current one.
    * easily tweak themes to support Multilingual mode
    * renders in a single swift, no need for two config files.
    
    Adds a couple of variables useful for multilingual sites
    
    Adds documentation (content/multilingual.md)
    
    Added language prefixing for all URL generation/permalinking see in the
    code base.
    
    Implements i18n. Leverages the great github.com/nicksnyder/go-i18n lib.. thanks Nick.
    * Adds "i18n" and "T" template functions..

--- tpl/template_funcs.go
+++ tpl/template_funcs.go
@@ -1884,5 +1884,7 @@ func init() {
 		"upper":        func(a string) string { return strings.ToUpper(a) },
 		"urlize":       helpers.URLize,
 		"where":        where,
+		"i18n":         I18nTranslate,
+		"T":            I18nTranslate,
 	}
 }
